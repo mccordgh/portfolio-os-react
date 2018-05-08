6b4a18582611acfcdaca3cca528dc53f41ab863d
Matthew McCord <mattm.eternal@gmail.com>
2018-05-08 08:36:29 -0500

    Create Basic Banner components and css

---
+++ src/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Banner.css';
+
+export default class Banner extends Component {
+  render() {
+    return (
+      <div className="banner__container">
+       <span>BANNER</span> 
+      </div>
+    );
+  }
+}
