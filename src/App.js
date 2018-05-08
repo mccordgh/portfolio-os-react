f94d1e95ea53d0ef76dcf7ca3c89292bbd9463e5
Matthew McCord <mattm.eternal@gmail.com>
2018-05-08 08:35:37 -0500

    Update main App component to include Banner/Desktop/Footer components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Banner from './Banner/Banner.js';
+import Desktop from './Desktop/Desktop.js';
+import Footer from './Footer/Footer.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Banner />
+        <Desktop />
+        <Footer />
       </div>
     );
   }
