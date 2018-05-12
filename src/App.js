6c19aca083a9143080059993ebc603dda6d3341c
Matthew McCord <mattm.eternal@gmail.com>
2018-05-12 14:09:18 -0500

    Remove unused Footer component import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Banner from './Banner/Banner.js';
 import Desktop from './Desktop/Desktop.js';
-import Footer from './Footer/Footer.js';
 import './App.css';
 
 class App extends Component {
