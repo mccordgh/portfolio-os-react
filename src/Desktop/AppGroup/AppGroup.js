c9479ee626b2f3208ad75be1fe057258674984ce
Matthew McCord <mattm.eternal@gmail.com>
2018-05-12 14:08:31 -0500

    Add functionality to read in json for Desktop component

--- src/Desktop/AppGroup/AppGroup.js
+++ src/Desktop/AppGroup/AppGroup.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import OsApp from './OsApp/OsApp';
 import './AppGroup.css';
 
 export default class AppGroup extends Component {
   render() {
     return (
       <div className="AppGroup">
-        <div className="AppImage" style={{backgroundImage: `linear-gradient(to bottom right, ${this.props.bgColor} 30%, white 150%)`}}>
+        <div className="AppGroupBackground">
         </div>
-        <div className="AppTitle">
+        <div className="AppGroupTitle">
           <h3>{ this.props.name }</h3>
         </div>
       </div>
--- src/Desktop/Desktop.js
+++ src/Desktop/Desktop.js
@@ -1,23 +1,65 @@
 import React, { Component } from 'react';
 import AppGroup from './AppGroup/AppGroup';
-import apiKey from '../key.json';
+
+// import apiKey from '../key.json';
+import appsList from '../json/apps';
+
 import headIcon from '../res/mccordinator2_head.png';
+
 import './Desktop.css';
 
+// TODO: IN ABOUT SECTION DO A FAQ
+
 export default class Desktop extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      apps: [],
+    };
+  }
+
   render() {
     return (
       <div className="desktop__container">
         <div className="appgroup__container">
-          <AppGroup name="games" bgColor="red"/>
-          <AppGroup name="apps" bgColor="green"/>
-          <AppGroup name="music" bgColor="orange"/>
-          <AppGroup name="about" bgColor="blue"/>
+          {
+            this.state.apps.map((appGroup, key) => {
+              return <AppGroup name={appGroup.name} list={appGroup.list} />;
+            })
+          }
         </div>
         <div className="os__image">
-          <img src={headIcon} />
+          <img src={headIcon} alt="Mccordinator's Pixel Head"/>
         </div>
       </div>
     );
+  }
+
+  componentDidMount() {
+    this.setState({apps: this.getAppsInfo()});
+    console.log(this.state);
+  }
+
+  getAppsInfo() {
+    const aa = this.setAppsBgColors(appsList.data);
+    console.log({aa})
+    return aa;
+  }
+
+  setAppsBgColors(data) {
+    const dataMap = data.map(appGroup => {
+      appGroup.list = appGroup.list.map(item => Object.assign({}, item, {bgColor: this.getRandomColor()}));
+      return appGroup;
+    });
+    console.log({dataMap})
+    return dataMap;
+  }
+
+  getRandomColor() {
+    const colors = ['red', 'green', 'orange', 'blue', 'purple', 'gold']
+    const rndColor = Math.floor(Math.random() * 6);
+
+    return colors[rndColor];
   }
 }
