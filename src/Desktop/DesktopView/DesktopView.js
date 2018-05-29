853208df53caf20d39c7b4cc056b461afec4c758
Matthew McCord <mattm.eternal@gmail.com>
2018-05-29 18:58:38 -0500

    Create DesktopView component

---
+++ src/Desktop/DesktopView/DesktopView.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import FolderAppGroup from './FolderAppGroup/FolderAppGroup';
+
+import './DesktopView.css';
+
+export default class DesktopView extends Component {
+    render() {
+        return (
+            <div className="desktopContainer">
+                <div className="folderGroupContainer">
+                    {
+                        this.props.apps.map((appGroup, key) => {
+                        return <FolderAppGroup
+                            key={key}
+                            name={appGroup.name}
+                            list={appGroup.list}
+                            openAppCallback={this.props.openAppCallback}
+                        />;
+                        })
+                    }
+                </div>
+            </div>
+        );
+    }
+}
---
+++ src/Desktop/DesktopView/FolderAppGroup/FolderAppGroup.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class FolderAppGroup extends Component {
+    render() {
+        return (
+            <h1>HIIIIIII</h1>
+        );
+    }
+}
