daab4bae4794f4b1dc4f6f68a22215dfff25eef9
Matthew McCord <mattm.eternal@gmail.com>
2018-06-06 00:24:50 -0500

    basic Mccordinator head component and bubblez

---
+++ src/Desktop/Mccordinator/Mccordinator.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import './Mccordinator.css';
+
+const IMAGE_PATH = '/res';
+
+export default class Mccordinator extends Component {
+    constructor() {
+        super();
+    
+        this.state = {
+            bubbleText: '',
+            showBubble: false, 
+        };
+
+        this.clickyTheFace = this.clickyTheFace.bind(this);
+    }
+
+    render() {
+        return (
+            <div onClick={this.clickyTheFace} className="mccordinatorHead">
+                { this.state.showBubble && 
+                    <div className="mccordinatorSpeechBubble">
+                        <p>
+                            { this.state.bubbleText }
+                        </p>
+                        <hr />
+                        <span> + Yes </span>
+                        <span> + No </span>
+                    </div>
+                }
+            </div>
+        );
+    }
+
+    nextDialogueShow() {
+        this.setState({bubbleText: 'It looks like you are trying to browse this portfolio. Would you like me tell you just how awesome Matthew McCord is?'});
+        this.setState({showBubble: true});
+    }
+   
+    clickyTheFace() {
+        this.nextDialogueShow();
+    }
+}
