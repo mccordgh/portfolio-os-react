0b881dd0e7382d88027e4ff3a68fb2493857c607
Matthew McCord <mattm.eternal@gmail.com>
2018-09-30 22:10:53 -0700

    Updates

---
+++ precache-manifest.524cf981dac9ee831515cf3d83793e1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "907c5d2910369fc8825712347d22ce3d",
+    "url": "/static/media/mccordinator2_head.907c5d29.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9d16328a4679ed11a5b6",
+    "url": "/static/js/main.9d16328a.chunk.js"
+  },
+  {
+    "revision": "4241b9e268c478b25f00",
+    "url": "/static/js/1.4241b9e2.chunk.js"
+  },
+  {
+    "revision": "9d16328a4679ed11a5b6",
+    "url": "/static/css/main.24d70209.chunk.css"
+  },
+  {
+    "revision": "e1f5f390ef498f0eb90d514fa7c332ae",
+    "url": "/index.html"
+  }
+];
