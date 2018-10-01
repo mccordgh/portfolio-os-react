0068c0d60c6a171eb651b9eb90c399518a411d4b
Matthew McCord <mattm.eternal@gmail.com>
2018-09-30 22:18:08 -0700

    Updates

---
+++ precache-manifest.620e915313fa97c61905fc60e559b48f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "907c5d2910369fc8825712347d22ce3d",
+    "url": "//portfolio-os-react/static/media/mccordinator2_head.907c5d29.png"
+  },
+  {
+    "revision": "451a896d9f7172b91d54",
+    "url": "//portfolio-os-react/static/js/runtime~main.451a896d.js"
+  },
+  {
+    "revision": "1a7a87b1030bda3940e3",
+    "url": "//portfolio-os-react/static/js/main.1a7a87b1.chunk.js"
+  },
+  {
+    "revision": "4241b9e268c478b25f00",
+    "url": "//portfolio-os-react/static/js/1.4241b9e2.chunk.js"
+  },
+  {
+    "revision": "1a7a87b1030bda3940e3",
+    "url": "//portfolio-os-react/static/css/main.7ca6e7d0.chunk.css"
+  },
+  {
+    "revision": "29f828495ec190abf470ee194e0f659c",
+    "url": "//portfolio-os-react/index.html"
+  }
+];
