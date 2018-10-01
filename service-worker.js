16d0305d1d031afc91720161718a360bc360c3d2
Matthew McCord <mattm.eternal@gmail.com>
2018-10-01 16:05:49 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ importScripts("/portfolio-os-react/workbox-v3.6.2/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/portfolio-os-react/workbox-v3.6.2"});
 
 importScripts(
-  "/portfolio-os-react/precache-manifest.8e6f9c5cbfb686adbd2f239ad2a318f2.js"
+  "/portfolio-os-react/precache-manifest.ecb9b4a63c1eedfdff01b510b4be0c66.js"
 );
 
 workbox.clientsClaim();
