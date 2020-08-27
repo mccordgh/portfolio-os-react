47ac2f359483015a0e19773bc192b69fdd9b197f
Matthew McCord <mattm.eternal@gmail.com>
2020-08-26 22:51:51 -0700

    Updates

---
+++ precache-manifest.a3102975eb674460352f364cc1db4789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b68ada2d113357405f47397de321fc7",
+    "url": "/portfolio-os-react/index.html"
+  },
+  {
+    "revision": "c8386e9c77758be261a7",
+    "url": "/portfolio-os-react/static/css/main.9ea22715.chunk.css"
+  },
+  {
+    "revision": "a7fc910fe3544e5664e9",
+    "url": "/portfolio-os-react/static/js/2.e4756d1c.chunk.js"
+  },
+  {
+    "revision": "c8386e9c77758be261a7",
+    "url": "/portfolio-os-react/static/js/main.ca6afa9e.chunk.js"
+  },
+  {
+    "revision": "13ff45e898a8e8061f4f",
+    "url": "/portfolio-os-react/static/js/runtime-main.6b2ce0ef.js"
+  },
+  {
+    "revision": "907c5d2910369fc8825712347d22ce3d",
+    "url": "/portfolio-os-react/static/media/mccordinator2_head.907c5d29.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,14 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("/portfolio-os-react/workbox-v3.6.2/workbox-sw.js");
-workbox.setConfig({modulePathPrefix: "/portfolio-os-react/workbox-v3.6.2"});
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-os-react/precache-manifest.4049bcede59dfdd0d38760dac33721d2.js"
+  "/portfolio-os-react/precache-manifest.a3102975eb674460352f364cc1db4789.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -26,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio-os-react/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-os-react/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
